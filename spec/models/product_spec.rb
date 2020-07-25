8d674a24f680fa18bb955dd723ead6f647e3a7b9
Saurabh Shinde <shindesaurabh824@gmail.com>
2020-07-25 16:39:40 +0530

    Added rspec tests for product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'schema' do
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+  end
+
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:product)).to be_valid
+  end
+
+  it 'is valid with a name' do
+    expect(create(:product)).to be_valid
+  end
+
+  it 'is invalid without an name' do
+    product = FactoryBot.build(:product, name: nil)
+    product.valid?
+    expect(product.errors[:name]).to include(I18n.t('test.errors.blank'))
+  end
+end
