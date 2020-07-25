3a2c5d6b34ed6c5a534e5a8058fd00cbe5e2b9c1
Saurabh Shinde <shindesaurabh824@gmail.com>
2020-07-25 20:45:27 +0530

    Exception handling done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::ParameterMissing do
+    render_error(t('errors.no_items_select'))
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render_error(t('errors.not_found'))
+  end
+
+  private
+
+  def render_error(message)
+    flash[:danger] = message
+    redirect_to root_path
+  end
 end
